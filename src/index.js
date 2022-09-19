a435ef0d99cd560a9ce3108dfb5ba6a73276bf2a
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-09-19 12:25:29 +0400

    main

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Header from './components/Header';
 import Experience from './components/Experience';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
