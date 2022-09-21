d4f06202731bfabce51bdcda5d1a5290b37eba2f
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-09-21 17:53:48 +0400

    main

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className="App">
-        <div className="mainContainer">
-            sadasd
-        </div>
+    <div className="contact_container">
+      <div className="content">
+        <h2>Contact Information</h2>
+      </div>
+      <div className="map">
+        <img src="/images/contact/map.png" alt="img" />
+      </div>
     </div>
   )
 }
