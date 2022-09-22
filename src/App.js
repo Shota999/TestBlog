bd5a12196ef2bf01449ff2e1e3b0fc2d3cd7bcbb
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-09-22 21:24:41 +0400

    main

--- src/App.js
+++ src/App.js
@@ -9,33 +9,35 @@ import News from './components/News';
 import Contact from './components/Contact';
 
 function App() {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
 
-      <Header />
+  <Header />
 
-      <div className="App">
-        <div className="mainContainer">
-          <div className="left">
-            <h1>We SEO Company That Will Help Your Business Become <span>Successful</span></h1>
-            <p>Our professional support team try to do their best to provide you with <span>high-quality</span> consultations concerning any question about our services.</p>
-            <button className="get_button">get in touch</button>
-          </div>
-          <div className="right">
-            <img src="https://ld-wp.template-help.com/wordpress_free/23137/wp-content/uploads/2019/03/home-img-1.png" alt="img" />
-          </div>
-        </div>
+  <div className="App">
+    <div className="mainContainer">
+      <div className="left">
+        <h1>We SEO Company That Will Help Your Business Become <span>Successful</span></h1>
+        <p>Our professional support team try to do their best to provide you with <span>high-quality</span>
+          consultations concerning any question about our services.</p>
+        <button className="get_button">get in touch</button>
       </div>
+      <div className="right">
+        <img src="https://ld-wp.template-help.com/wordpress_free/23137/wp-content/uploads/2019/03/home-img-1.png"
+          alt="img" />
+      </div>
+    </div>
+  </div>
 
-      <Experience />
-      <Services />
-      <Company />
-      <Advantage />
-      <News />
-      <Contact />
-    </>
-  );
+  <Experience />
+  <Services />
+  <Company />
+  <Advantage />
+  <News />
+  <Contact />
+</>
+);
 }
 
 export default App;
